e9478de0f6f2748f38569478ec14f20b44815d49
Fu Hsien <fuhsienng@gmail.com>
2017-03-31 15:32:20 +0800

    .

--- js/addin.js
+++ js/addin.js
@@ -331,7 +331,7 @@ geotab.addin.geotabFuelSensor = function(api, state) {
                 //indicates next point is the new activity
                 index += Math.floor(counter/2);
                 console.log("REFILL FOUND @: ",index,new Date(time[index]));
-                console.log("Amount filled: ",theftCount[i+avgPoints][3]);
+                console.log("Amount filled: ",i+avgPoints,theftCount[i+avgPoints]);
                 newflag = 1;
                 counter = 0;
             }
