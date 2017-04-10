928e0bbe018cf667a03423f6f6fe4439d57fed67
Fu Hsien <fuhsienng@gmail.com>
2017-04-10 12:25:04 +0800

    .

--- js/addin.js
+++ js/addin.js
@@ -187,15 +187,14 @@ geotab.addin.geotabFuelSensor = function(api, state) {
                 // Adding sessions into fuel array
                 console.log("Length before appending", fuelSessions.length, drivingSessions.length);
                 var comparator = drivingSessions[0].getTime();
-                console.log("check time",typeof comparator,typeof drivingSessions[0]);
-                /*for (i=0,j=1;i<fuelSessions.length;i++){
+                for (i=0,j=1;i<fuelSessions.length;i++){
                     var fuelTime = new Date(fuelSessions[i].dateTime).getTime();
-                    if(comparator - fuelTime<0){
+                    /*if(comparator - fuelTime<0){
                         fuelSessions.splice(i++,0,null);
                         comparator = drivingSessions[j++].getTime();
-                    }
+                    }*/
                 }
-                console.log("After appending",fuelSessions.length);*/
+                console.log("After appending",fuelSessions.length);
                 callback1(results, callback2, vehicleID); //plotData,callback2:createtable
 
             });
