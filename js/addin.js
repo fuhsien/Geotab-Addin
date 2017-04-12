0800cad0442c5a1fbe07edf1ddb922cd58ae9f2d
Fu Hsien <fuhsienng@gmail.com>
2017-04-12 15:57:08 +0800

    .

--- js/addin.js
+++ js/addin.js
@@ -45,7 +45,7 @@ geotab.addin.geotabFuelSensor = function(api, state) {
         sFlag = 0, //check if start date selected
         eFlag = 0, //check if end date selected
         avgPoints = 28,
-        fuelThreshold = 10,
+        fuelThreshold = 5,
         tankSize = 80,
         selectedOpt,
         startPicker,
