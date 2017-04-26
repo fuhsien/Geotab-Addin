b48ac5af5ed4ac9ec3b1e71f7ebac7f8c0677c52
Fu Hsien <fuhsienng@gmail.com>
2017-04-26 10:12:38 +0800

    .

--- carFuelEfficiency/js/main.js
+++ carFuelEfficiency/js/main.js
@@ -22,114 +22,97 @@
 
  */
 geotab.addin.addinTemplate = function(api, state) {
-
 	/*********************************Global variables***********************************/
 	var fromSheet;
 	/************************************************************************************/
-
-
 	/**********************************Retrieve data*************************************/
 	//API call for fuel data
 	//Pull JSON from Google Sheet
-    var initializeJSON = function() {
-        var spreadsheetID = "1VBDZZoYqCSWV3ABO7-eBqb21WQjgPLkO3uOBtAQsnr8";
-        //var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/od6/public/values?alt=json";
-        var url = "https://script.google.com/macros/s/AKfycbygukdW3tt8sCPcFDlkMnMuNu9bH5fpt7bKV50p2bM/exec?id=" + spreadsheetID + "&sheet=Sheet1";
-        $.getJSON(url, function(data) {
-            // loop to build html output for each row
-            fromSheet = data.Sheet1;
-            console.log("fromSheet", fromSheet);
-        });
-        /*$.getJSON(url, function(data) {
-            // loop to build html output for each row
-            var entry = data.feed.entry;
-            var line = entry[0]['gsx$device']['$t'];
-            var test = entry[0].content;
-        });*/
-        console.log("Loaded Google Sheet");
-    };
+	var initializeJSON = function() {
+		var spreadsheetID = "1VBDZZoYqCSWV3ABO7-eBqb21WQjgPLkO3uOBtAQsnr8";
+		//var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/od6/public/values?alt=json";
+		var url = "https://script.google.com/macros/s/AKfycbygukdW3tt8sCPcFDlkMnMuNu9bH5fpt7bKV50p2bM/exec?id=" + spreadsheetID + "&sheet=Sheet1";
+		$.getJSON(url, function(data) {
+			// loop to build html output for each row
+			fromSheet = data.Sheet1;
+			console.log("fromSheet", fromSheet);
+		});
+		/*$.getJSON(url, function(data) {
+		    // loop to build html output for each row
+		    var entry = data.feed.entry;
+		    var line = entry[0]['gsx$device']['$t'];
+		    var test = entry[0].content;
+		});*/
+		console.log("Loaded Google Sheet");
+	};
 	var getVehicles = function(finishedCallback) {
-        api.call("Get", {
-            typeName: "Device"
-        }, function(results) {
+		api.call("Get", {
+			typeName: "Device"
+		}, function(results) {
 			if (results.length === 0) {
 				throw "No vehicle found!";
 			}
-            //console.log("Device", results);
-            vehicles = results.map(function(vehicle) {
-                return {
-                    name: vehicle.name,
-                    id: vehicle.id,
-                    serialNumber: vehicle.serialNumber
-                };
-            });
-            //console.log("Vehicles loaded", vehicles);
-
+			//console.log("Device", results);
+			vehicles = results.map(function(vehicle) {
+				return {
+					name: vehicle.name,
+					id: vehicle.id,
+					serialNumber: vehicle.serialNumber
+				};
+			});
+			//console.log("Vehicles loaded", vehicles);
 			document.getElementById("fuelEff-container").style.display = "block";
-            finishedCallback();
-        }, function(errorString) {
-        	throw "Error while trying to load vehicles. " + error;
-        });
-    };
-
-    var getFuel = function(){
-    	api.call("Get", {
-            "typeName": "Diagnostic",
-            "search": {
-                "name": "Analog aux 1"
-            },
-        },function(result){
-        	//Report for this month
-        	var auxID = result[0].id;
-        	var reportStart = new Date();
-        	var reportEnd = new Date();
-
-        	reportStart.setDate(1);
+			finishedCallback();
+		}, function(errorString) {
+			throw "Error while trying to load vehicles. " + error;
+		});
+	};
+	var getFuel = function() {
+		api.call("Get", {
+			"typeName": "Diagnostic",
+			"search": {
+				"name": "Analog aux 1"
+			},
+		}, function(result) {
+			//Report for this month
+			var auxID = result[0].id;
+			var reportStart = new Date();
+			var reportEnd = new Date();
+			reportStart.setDate(1);
 			reportStart.setHours(0);
 			reportStart.setMinutes(0);
 			reportStart.setSeconds(0);
 			reportStart.setMilliseconds(0);
-
-        	api.call("Get", {
-	            "typeName": "Diagnostic",
-	            "search": {
-	                diagnosticSearch: {
+			api.call("Get", {
+				"typeName": "Diagnostic",
+				"search": {
+					diagnosticSearch: {
 						"id": auxID
 					},
 					fromDate: reportStart,
 					toDate: reportEnd
-	            }, function(results){
-	            	console.log("Diagnostic data ",results);
-	            }, function(errorString){
-	            	throw "Error retrieving fuel data. " + error;
-	            }
-            });
-
-        });
-    };
+				},
+			}, function(results) {
+				console.log("Diagnostic data ", results);
+			}, function(errorString) {
+				throw "Error retrieving fuel data. " + error;
+			});
+		});
+	};
 	/************************************************************************************/
-
-
 	/*******************************Results presentation*********************************/
-	
 	/************************************************************************************/
-
 	return {
-	    initialize: function(api, state, initializeCallback) {
+		initialize: function(api, state, initializeCallback) {
 			initializeJSON();
 			getVehicles(initializeCallback);
-
-	    },
-
-        /**
-         * focus() is called whenever the Add-In receives focus.
-         */
-	    focus: function(api, state) {
-			var test = state.getState();
-			console.log(test);
+		},
+		/**
+		 * focus() is called whenever the Add-In receives focus.
+		 */
+		focus: function(api, state) {
 			getFuel();
 		},
-		
 		/**
 		 * blur() is called whenever the user navigates away from the Add-In.
 		 */
